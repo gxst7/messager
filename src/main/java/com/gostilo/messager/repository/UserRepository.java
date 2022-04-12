b6e0d703e3d0824479eda730dbb43a826c10d969
Alexander Gostilo <alexandergostilo@gmail.com>
2022-04-12 14:42:23 +0300

    add repository annotation

--- src/main/java/com/gostilo/messager/repository/UserRepository.java
+++ src/main/java/com/gostilo/messager/repository/UserRepository.java
@@ -2,9 +2,11 @@ package com.gostilo.messager.repository;
 
 import com.gostilo.messager.entity.User;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface UserRepository extends CrudRepository<User, Long> {
 
     List<User> findByLastName(String lastName);
