0b51e1a9ec123e69cc4014da02d2bda9da7fca81
Alexander Gostilo <alexandergostilo@gmail.com>
2022-04-12 13:42:55 +0300

    divided main controller for two mappings

--- src/main/java/com/gostilo/messager/controllers/MainController.java
+++ src/main/java/com/gostilo/messager/controllers/MainController.java
@@ -7,7 +7,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MainController {
 
-    @GetMapping({"/", "/home"})
+    @GetMapping("/")
+    public String index() {
+        return "Hello There";
+    }
+
+    @GetMapping("/home")
     public String home(@RequestParam(value = "name", defaultValue = "World") String name) {
         return String.format("Hello %s!", name);
     }
