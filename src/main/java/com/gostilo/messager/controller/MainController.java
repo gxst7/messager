65d52a9e7c39b0c0c0e187022033781e395e0ea2
Alexander Gostilo <alexandergostilo@gmail.com>
2022-04-12 19:40:49 +0300

    set up forward to thymeleaf template

--- src/main/java/com/gostilo/messager/controller/MainController.java
+++ src/main/java/com/gostilo/messager/controller/MainController.java
@@ -1,19 +1,20 @@
 package com.gostilo.messager.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class MainController {
 
     @GetMapping("/")
-    public String index() {
-        return "Hello There";
+    public String index(Model model) {
+        return "index";
     }
 
     @GetMapping("/home")
-    public String home(@RequestParam(value = "name", defaultValue = "World") String name) {
-        return String.format("Hello %s!", name);
+    public String home(@RequestParam(value = "name", defaultValue = "World") String name, Model model) {
+        return "home";
     }
 }
