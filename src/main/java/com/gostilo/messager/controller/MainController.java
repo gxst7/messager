80f0441fc91ab0a522590498ecca12d73c8d8213
Alexander Gostilo <alexandergostilo@gmail.com>
2022-04-15 12:42:12 +0300

    add file loading

--- src/main/java/com/gostilo/messager/controller/MainController.java
+++ src/main/java/com/gostilo/messager/controller/MainController.java
@@ -4,15 +4,24 @@ import com.gostilo.messager.domain.Message;
 import com.gostilo.messager.domain.User;
 import com.gostilo.messager.repository.MessageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
 
 @Controller
 public class MainController {
+
+    @Value("${upload.path}")
+    private String uploadPath;
 
     @Autowired
     private MessageRepository messageRepository;
@@ -42,10 +51,28 @@ public class MainController {
             @AuthenticationPrincipal User user,
             @RequestParam String text,
             @RequestParam String tag,
-            Model model) {
+            @RequestParam("file") MultipartFile file,
+            Model model) throws IOException {
         Message message = new Message(text, tag, user);
+
+        if (!file.isEmpty()) {
+            File uploadDir = new File(uploadPath);
+
+            if (!uploadDir.exists()) {
+                uploadDir.mkdir();
+            }
+
+            String uuidFile = UUID.randomUUID().toString();
+            String resultFilename = uuidFile + "." + file.getOriginalFilename();
+
+            file.transferTo(new File(resultFilename));
+
+            message.setFilename(resultFilename);
+        }
+
         messageRepository.save(message);
         Iterable<Message> messages = messageRepository.findAll();
+        model.addAttribute("filename", message.getFilename());
         model.addAttribute("messages", messages);
         return "home";
     }
